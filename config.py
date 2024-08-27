ef2d54993af2136af32558dc2e02fb993920057a
lev <ivanovleva512@gmail.com>
2024-08-28 00:08:50 +0700

    Completed Task Async

---
+++ config.py
@@ -0,0 +1,6 @@
+PG_USER = 'async_user'
+PG_PASSWORD = 'password'
+PG_HOST = '127.0.0.1'
+PG_DB = 'async_db'
+PG_DSN = f'postgresql://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:5432/{PG_DB}'
+PG_DSN_ALC = f'postgresql+asyncpg://{PG_USER}:{PG_PASSWORD}@{PG_HOST}:5432/{PG_DB}'
